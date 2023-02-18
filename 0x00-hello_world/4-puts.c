24d8e2481977a91c6c088f1f6c110ce37fc60f26
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 17:49:20 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-ltrace puts "Programming is like building a multilingual puzzle" /n
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts ("Programming is like building a multilingual puzzle \n") 
+    return (0);
+}
+
