abff7e2d9058bd06fe229100dd780c4d09ce7059
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 20:43:44 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,8 +7,8 @@
  * Return: Always 1 (failure)
  */
  
-int main(void)
-{
-write(stderr,"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",30);
-return (1);
+int main(void) {
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 37);
+return 1;
 }
