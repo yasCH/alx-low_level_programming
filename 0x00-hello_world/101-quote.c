5c4a5c11f5173a42ae522cd4097f1d37811388fe
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 20:16:52 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  
 int main(void)
 {
-fprintf(stderr,"%d \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",x);
+fprintf(stderr,"%d \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
