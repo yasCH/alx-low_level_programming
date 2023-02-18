13d6598e53cce5aba82860699b72a951a2c50e0a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 18:34:17 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
  */
 int main(void)
 {
-writ("64");
+write("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n\n
+Size of a long int: 4 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n");
 return (0);
 }
