aa5dae9b002a7541ca98899aeced24a99deb5b74
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 19:21:20 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
