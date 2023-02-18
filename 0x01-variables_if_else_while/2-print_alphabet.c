1640fa2a17d0640782af0193f4484f7148a431cb
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 22:55:02 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,18 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'print the alphabet in lowercase, followed by a new line using putchar.'
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char alpha = 'a';
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+putchar(alpha);
+return (0);
+}
