8a364a80f817daab44a60e3248ea9839c71aa3c8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 17:50:55 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-    puts ("Programming is like building a multilingual puzzle \n") 
+    puts ("Programming is like building a multilingual puzzle \n");
     return (0);
 }
 
