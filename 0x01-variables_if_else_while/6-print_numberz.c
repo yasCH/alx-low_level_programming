e7c434fe84eb44d7f3716a4dc9285835cd97dca8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 00:56:37 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,22 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'prints all single digit numbers of base 10 starting from 0,
+* followed by a new line.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int number = 0;
+for (number = 0 ; number <= 9 ; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
