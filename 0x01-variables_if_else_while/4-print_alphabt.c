178a011e4bb296e71909984bcb543638084480f4
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 00:25:53 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,22 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'print the alphabet in lowercase exept q & e,
+* followed by a new line 
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char alpha = 'a';
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
