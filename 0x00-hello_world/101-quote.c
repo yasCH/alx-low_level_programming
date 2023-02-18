a103aa9ff66e504f2334b260666238a39909299d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 20:50:22 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  *
  * Return: Always 1 (failure)
  */
-int main(void) {
-char *message ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int main(void) 
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, message, 59);
 return (1);
 }
