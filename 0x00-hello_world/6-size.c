80ef9ea2c5ae6b57fe5dcf0dac0aad6ef2c7d4ac
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 18:31:49 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n
+writ("Size of a char: 1 byte(s)\n
 Size of an int: 4 byte(s)\n
 Size of a long int: 4 byte(s)\n
 Size of a long long int: 8 byte(s)\n
