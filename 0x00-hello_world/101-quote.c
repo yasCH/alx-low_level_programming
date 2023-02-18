48ec161b77692c471d892b165d92ee9a395b04a0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-18 20:58:41 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 char *message = "and that piece of art is useful\" - Dora";
 char *messages = " Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message + messages, 59);
+write(STDERR_FILENO, message messages, 59);
 return (1);
 }
