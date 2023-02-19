5baff0301d039f13aaa0100463770eefaf816e33
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 15:54:08 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,24 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'prints all possible combinations of single-digit numbers.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int number = 0;
+int comma = 44;
+int space = 32;
+for (number = 0 ; number <= 9 ; number++)
+{
+putchar(number);
+putchar (comma);
+putchar (space);
+}
+return (0);
+}
