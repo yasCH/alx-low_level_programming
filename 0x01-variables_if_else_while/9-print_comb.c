d8bcd45287d1623c587e3c0f46c660389a868e86
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 16:18:28 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,16 @@ int main(void)
 int number = 48;
 int comma = 44;
 int space = 32;
-for (number = 48 ; number < 57 ; number++)
+for (number = 48 ; number <= 58 ; number++)
 {
+if (number > 57)
+{
+putchar(\n);
+break;
+}
 putchar(number);
 putchar (comma);
 putchar (space);
 }
-putchar(number);
 return (0);
 }
