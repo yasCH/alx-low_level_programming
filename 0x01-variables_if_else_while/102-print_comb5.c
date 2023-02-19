8b3c2d2db7a974687b8403c27cbeeef3e1af9b7c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 19:24:20 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+**main - Entry point
+*
+* Description: 'prints all possible different combinations of three digits.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+return (0);
+}
