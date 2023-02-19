a7b4fd57f932b2ed0b4c15c54e7d0b58928c1174
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 19:32:39 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,11 +5,15 @@
 /**
 **main - Entry point
 *
-* Description: 'prints all possible different combinations of three digits.
+* Description: 'prints all possible combinations of two two-digit numbers.
 *
 * Return: Always 0 (success)
 */
 int main(void)
 {
+int number = 48;
+int numbernd = 48;
+int comma = 44;
+int space = 32;
 return (0);
 }
