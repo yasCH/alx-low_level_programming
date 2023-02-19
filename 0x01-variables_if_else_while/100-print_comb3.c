d5147bf29dcba334bd5559dae3947bc9f4e45af0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 18:15:06 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+**main - Entry point
+*
+* Description: 'prints all possible different combinations of two digits.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int numbernd = 49;
+int number = 48;
+int comma = 44;
+int space = 32;
+for (number = 48 ; number <= 57 ; number++)
+{
+for (numbernd = 49 ; numbernd <= 57 ; numbernd++)
+{
+putchar(number);
+putchar(numbernd);
+putchar(comma);
+putchar(space);
+}
+}
+putchar(\n);
+return (0);
+}
