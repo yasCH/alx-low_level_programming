d3d7051dc79cc9007824c287d5def9c72071070d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 15:24:05 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,22 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'prints  all the numbers of base 16 in lowercase,
+* followed by a new line.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int number = 30;
+for (number = 30 ; number <= 39 ; number++)
+{
+putchar(number);
+}
+putchar('\n');
+return (0);
+}
