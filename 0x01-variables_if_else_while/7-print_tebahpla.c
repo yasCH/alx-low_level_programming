776c54fe4195496717264c1d179bde61fe46aba2
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 14:34:43 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,22 @@
-#!/bin/bash
- 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+**main - Entry point
+*
+* Description: 'prints lowercase alphabet in reversel,
+* followed by a new line.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char alpha = 'z';
+for (alpha = 'z' ; alpha <= a ; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
