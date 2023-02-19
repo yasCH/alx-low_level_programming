d20d3a5f5a89ad55181fcb370c9a3a2ee46361be
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 23:19:09 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,23 @@ int main(void)
   int numberB = 48;
   int numberC = 48;
   int numberD = 48;
+  int numberE = 48;
   int comma = 44;
   int space = 32;
   for (numberA = 48 ; numberA <= 57 ; numberA++)
   {
     for (numberB = 48 ; numberB <= 57 ; numberB++)
     { 
+      numberE = numberB + 1
       for (numberC = 48 ; numberC <= 57 ; numberC++)
         {
-          for (numberD = numberB+ 1 ; numberD <= 57 ; numberD++)
-          {
+        
+          for (numberD = numberE ; numberD <= 57 ; numberD++)
+          { 
+            if (numberD <= 57)
+              {
+              numberD = 48
+              }
             if (numberA+numberB != numberC+numberD)
             {
               putchar(numberA);
