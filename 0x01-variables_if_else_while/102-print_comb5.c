fbf806d95dc5e3b5790f1dd91b7cd63f188edd3f
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 20:41:47 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,32 +11,26 @@
 */
 int main(void)
 {
-int number = 48;
-int numbernd = 48;
-int comma = 44;
-int space = 32;
-for (number = 48 ; number <= 57 ; number++)
-{
-for (numbernd = number + 1 ; numbernd <= 57 ; numbernd++)
-{
-if (number == numbernd)
-{
-continue;
-}
-putchar(number);
-putchar(numbernd);
-putchar(space);
-for (number = 48 ; number <= 57 ; number++)
-{
-for (numbernd = number + 1 ; numbernd <= 57 ; numbernd++)
-{
-putchar(number);
-putchar(numbernd);
-putchar(comma);
-putchar(space);
-}
-}
-}
-}
-return (0);
+  int numberA = 48;
+  int numberB = 48;
+  int numberC = 48;
+  int numberD = 48;
+  int comma = 44;
+  int space = 32;
+  for (numberA = 48 ; numberA <= 57 ; numberA++)
+  {
+    for (numberB = 48 ; numberB <= 57 ; numberB++)
+    {
+      putchar(space);
+      for (numberC = 48 ; numberC <= 57 ; numberC++)
+        {
+          for (numberD = 48 ; numberD <= 57 ; numberD++)
+          {
+            putchar(comma);
+            putchar(space);
+          }
+       }
+    }
+  }
+  return (0);
 }
