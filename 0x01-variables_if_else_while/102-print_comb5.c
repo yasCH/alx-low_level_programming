ef7805b4417b712a7ca2aac676bfeb89cc5b8cdf
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-19 19:38:42 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,5 +15,16 @@ int number = 48;
 int numbernd = 48;
 int comma = 44;
 int space = 32;
+for (number = 48 ; number <= 57 ; number++)
+{
+for (numbernd = number + 1 ; numbernd <= 57 ; numbernd++)
+{
+putchar(number);
+putchar(comma);
+putchar(numbernd);
+putchar(comma);
+putchar(space);
+}
+}
 return (0);
 }
