4f42041d3b1c2db03d8f802e42cda9ab7fdc49d1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-21 23:44:25 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+**islower - Prototype practicing
+*
+* Description:Write a function that checks for lowercase character.
+*
+* Return: (0)
+*/
+int _islower(int c)
+{
+if ((c >= a) && (c >= z))
+{
+return (0);
+}
+}
