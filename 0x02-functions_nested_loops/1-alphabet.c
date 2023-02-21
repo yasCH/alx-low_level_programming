569a2532192ed018a0bb20c69d61f4ef596134d7
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-21 18:19:35 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
+
+/**
+**main - Entry point
+*
+* Description:Write a function that prints the alphabet, in lowercase,
+* followed by a new line.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+  void print_alphabet();
+   getchar();
+   return 0;
+}
+void void print_alphabet()
+{
+   printf("void print_alphabet called");
+}
+char alpha = 'a';
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
