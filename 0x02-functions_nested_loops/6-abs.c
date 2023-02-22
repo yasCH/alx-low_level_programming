ac45fa88078f5fdd925d49c1a821f6f716fda8e0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 01:56:39 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+**_abs - Prototype practicing
+*
+* Description:Write a function that computes 
+* the absolute value of an integer.
+*
+* @v:character to check if it islowercase.
+*
+* Return:0
+*/
+
+int _abs(int v)
+{
+if (v < 0)
+{
+_putchar(v*(-1));
+}
+else
+{
+_putchar(v);
+}
+}
