55e3b157bd51aa5b4abcce44379d55975a3e9a15
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-23 00:07:29 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,9 +17,9 @@ void times_table(void)
 {
 int i = 0;
 int j = 0;
-for (i = 0, i <= 9, i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = i + 1, j <= 9, j++)
+for (j = i + 1; j <= 9; j++)
 {
 printf("%i",j);
 }
