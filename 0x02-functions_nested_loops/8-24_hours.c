5cf7da3454c520ebbf53eb627079665f54e6a80a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 17:52:38 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include "main.h"
 
+/**
+**jack_bauer - Prototype practicing
+*
+* Description:Write a function that cprints every minute of the day of Jack Bauer, 
+* starting from 00:00 to 23:59.
+* @a:character to check
+*
+* Return:a
+*/
+
+int jack_bauer(void)
+{
+int numberA = 48;
+int numberB = 48;
+int numberC = 48;
+int numberD = 48;
+
+int colomn = 58;
+for (numberA = 48 ; numberA <= 50 ; numberA++)
+{
+for (numberB = 48 ; numberB <= 51 ; numberB++)
+{
+for (numberC = 48 ; numberC <= 53 ; numberC++)
+{
+for (numberD = 48 ; numberD <= 57 ; numberD++)
+{
+putchar(numberA);
+putchar(numberB);
+putchar(colomn);
+putchar(numberC);
+putchar(numberD);
+putchar('\n');
+}
+}
+}
+}
+return (0);
+}
