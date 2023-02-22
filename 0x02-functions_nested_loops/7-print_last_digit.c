bbae005fc1f9b5a8cf8f55816f28decd4eca23bf
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 16:21:16 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+**print_last_digit - Prototype practicing
+*
+* Description:Write a function that prints the sign of a number.
+*
+* @l:character to check if it islowercase.
+*
+* Return: value of the last digit
+*/
+
+int print_last_digit(int)
+{
+int l;
+l= l % 10;
+return (l);
+}
 
