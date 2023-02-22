88fc6d158973515ccd4e5488de08be101ee80a2d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 23:39:16 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+**times_table - Prototype practicing
+*
+* Description:Write a function that prints the 9 times table, 
+* starting with 0.
+*
+* Return: (void)
+*/
+void times_table(void)
+{
+int i = 0 ,j;
+while (i <= 9)
+{
+j = 0;
+while (j <= 9)
+{
+printf(j);
+j++;
+}
+printf("\n");
+i++;
+}
+}
