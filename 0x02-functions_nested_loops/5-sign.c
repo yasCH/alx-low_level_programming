340acd96d11ccb15df90a6b24762513c7770b350
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 01:49:10 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include "main.h"
 /**
 **print_sign - Prototype practicing
 *
@@ -17,17 +17,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+_putchar(43);
 return (1);
 }
 else if (n == 0)
 {
-putchar(48);
+_putchar(48);
 return (0);
 }
 else
 {
-putchar(45);
+_putchar(45);
 return (-1);
 }
 }
