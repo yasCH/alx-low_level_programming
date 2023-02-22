fd0b472e0d8057c92fc92ebae3057f65c3ca944e
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-22 01:24:36 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+**print_sign - Prototype practicing
+*
+* Description:Write a function that prints the sign of a number.
+*
+* @n:character to check if it islowercase.
+*
+* Return:1 if n is greater than zero Return:0 if n is zero
+* Return:-1 if n is less than zero
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n = 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
