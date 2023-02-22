6b13f8845c4dee6f6f122245490c003e1bc24cee
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-23 00:46:55 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
+
+/**
+**add - Prototype practicing
+*
+* Description:Write a function that adds two integers and
+* returns the result.
+*
+* Return: (result)
+*/
+int add (int int)
+{
+int a;
+int b;
+int c = a + b;
+return (c);
+}
