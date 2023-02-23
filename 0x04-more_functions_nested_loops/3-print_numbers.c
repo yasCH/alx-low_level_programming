8fc66e2d3bbace53c4f61e12c515087e4a6afa6c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 00:47:07 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,8 +10,6 @@
 * Description:Write a function that prints numbers, 
 * from 0 to 9, followed by a new line.
 *
-* @number
-*
 * Return: (void)
 */
 void print_numbers(void)
@@ -20,6 +18,6 @@ int number = 48;
 for (number = 48; number <= 57; number++)
 {
 _putchar(number);
-}
 _putchar('\n');
+}
 }
