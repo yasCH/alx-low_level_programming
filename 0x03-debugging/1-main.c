f145887b3ab58f97031112c3a22ac5d02c635154
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-23 21:08:11 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,16 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0; 
 
-/**while (i < 10)
+/**
+*while (i < 10)
 *{
 *putchar(i);
-*}*/
+*}
+*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
