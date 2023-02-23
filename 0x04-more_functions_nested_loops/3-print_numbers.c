3509714933d2b78e5e59f126d33fb96ea712f3dd
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 00:02:58 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+**print_numbers - Prototype practicing
+*
+* Description:Write a function that prints numbers, 
+* from 0 to 9, followed by a new line.
+*
+* Return: (void)
+*/
+void print_numbers(void)
+{
+char number = 48;
+for (number = 48 ; number <= 57 ; number++)
+{
+putchar(alpha);
+}
+putchar('\n');
+}
+}
