0ceeb41289b912f45dbbe490dd92aa6fc94b8f3a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 00:59:39 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 char number = '0';
 for (number = '0'; number <= '9'; number++)
 {
-if (number != '2') || (number != '4'))
+if ((number != '2') || (number != '4'))
 {
 _putchar(number);
 }
