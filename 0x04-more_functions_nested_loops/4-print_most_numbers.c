184ca1c98e3a457cb7c3ca08101ad0658262ddfe
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 00:33:45 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,26 @@
 #include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "main.h"
+
 
 /**
 **print_most_numbers - Prototype practicing
 *
-* Description:Write a function that prints 10 times the alphabet,
-* in lowercase, followed by a new line.
+* Description:Write a function that prints the numbers from 0 to 9,
+* do not print two and four, followed by a new line.
 *
 * Return: (void)
 */
 void print_most_numbers(void)
 {
-
-putchar('\n');
+int number = 48;
+for (number = 48; number <= 57; number++)
+{
+if (number != 2) || (number != 4))
+{
+_putchar(number);
 }
+}
+_putchar('\n');
 }
