c3d830166409b4f666f038454d2b23bfcfb59bb0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-23 23:59:00 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+
+/**
+**mul - Prototype practicing
+*
+* Description:Write a function that multiplies two integers and
+* returns the result.
+*
+* @a:character to check
+* @b:character to check
+*
+* Return: (result)
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
