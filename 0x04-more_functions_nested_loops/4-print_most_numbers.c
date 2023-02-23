cf2e5b9a579d0f1b0ac37aa80bd97ae3dce05e10
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 00:28:18 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+**print_most_numbers - Prototype practicing
+*
+* Description:Write a function that prints 10 times the alphabet,
+* in lowercase, followed by a new line.
+*
+* Return: (void)
+*/
+void print_most_numbers(void)
+{
+
+putchar('\n');
+}
+}
