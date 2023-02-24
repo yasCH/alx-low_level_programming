7fbb1fec4988de0dca0be2ffca3088194d5407d7
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 01:13:41 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+**Prototype- Prototype practicing
+*
+* Description:
+*
+* @n
+*
+* Return:
+*/
+
+function
+{
+
+}
