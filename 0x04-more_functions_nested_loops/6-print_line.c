4147c6fc3377775faf618c242207dcd037faff82
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-25 00:13:59 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
-* void print_line - Write a function that draws a straight line 
+* print_line - Write a function that draws a straight line 
 * in the terminal.
 *
 * @n: character to check
