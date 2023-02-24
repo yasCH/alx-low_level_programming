0e0c4ffc8f18a95ed7f702b88e58d16d04d6f681
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 01:02:14 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
 **print_most_numbers - Prototype practicing
 *
-* Description:Write a function that prints the numbers from 0 to 9,
-* do not print two and four, followed by a new line.
+* Description:Write a function that prints the numbers from
+* 0 to 9, do not print two and four, followed by a new line.
 *
 * Return: (void)
 */
