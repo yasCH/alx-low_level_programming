e11033cd8a729a1050d0374c9a5a4e767a4ec3ec
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 17:43:11 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
-**print_most_numbers - Prototype practicing
-*
-* Description:Write a function that prints the numbers from
+* print_most_numbers :Write a function that prints the numbers from
 * 0 to 9, do not print two and four, followed by a new line.
 *
 * Return: (void)
 */
-
 void print_most_numbers(void)
 {
-for (number = 48; number <= 57; number++)
-{
-if (number != 50 || number != 52)
-{
-_putchar(number);
-}
-_putchar('\n');
-}
+  number = 48;
+  for (number = 48; number <= 57; number++)
+  {
+    if (number != 50 || number != 52)
+        _putchar(number);
+  }
+  _putchar('\n');
 }
