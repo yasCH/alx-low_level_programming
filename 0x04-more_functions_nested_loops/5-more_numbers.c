c60597cc91c9e3eb84e8d7cf7f15580fc6a5e4af
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-24 16:01:48 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@
 * Description: Write a function that prints 10 times the numbers,
 * from 0 to 14, followed by a new line.
 *
-* Return:
+* Return:0
 */
 
 void more_numbers(void);
@@ -25,4 +25,5 @@ printf("%d",number);
 }
 putchar('\n');
 }
+return(0);
 }
