d0ec02c7bc701108fa158434b1ac9b87c6daaa1c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-25 16:24:02 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,16 +5,33 @@
 #include "main.h"
 
 /**
-**Prototype- Prototype practicing
+* Description: Fizz Buzz
 *
-* Description:
-*
-* @n
-*
-* Return:
+* Return:(0)
 */
 
-function
+int main (void)
 {
-
+int p;
+for (p = 1 ;p <= 100; p++)
+{
+if (p % 3 = 0 &&  p % 5 = 0)
+{
+printf("FizzBuzz ");
+}
+else if (p % 3 = 0)
+{
+printf("Fizz ");
+}
+else if (p % 3 = 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf('%i ',p);
+}
+}
+putchar('\n');
+return (0);
 }
