7c97e5fa95f9bbf8717b1464181ce49a7fafd1a8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-25 16:34:29 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main (void)
 int p;
 for (p = 1 ;p <= 100; p++)
 {
-if (p % 3 = 0 &&  p % 5 = 0)
+if (p % 3 == 0 &&  p % 5 == 0)
 {
 printf("FizzBuzz ");
 }
-else if (p % 3 = 0)
+else if (p % 3 == 0)
 {
 printf("Fizz ");
 }
-else if (p % 5 = 0)
+else if (p % 5 == 0)
 {
 printf("Buzz ");
 }
@@ -34,3 +34,4 @@ printf("%i, ",p);
 putchar('\n');
 return (0);
 }
+
