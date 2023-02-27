ad77e77256cbe2ad0cd148c4c7afc6b0a04965f1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 14:53:55 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,11 @@
 *
 * Return: (void)
 */
-void print_rev(char *s)
+int length = strlen(s);
+int i;
+for (i = length - 1; i > 0; i--)
 {
-stringReverse(s);
+printf("%s", &(s[i]));
+}
+_putchar('\n');{
 }
