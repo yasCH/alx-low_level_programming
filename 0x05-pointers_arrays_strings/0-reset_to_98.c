fa15766a021ae2346708a5d494e2375358b31182
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 09:13:31 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
-* reset_to_98 - Write a function that takes a pointer to an int as parameter 
+* reset_to_98 - Write a function that takes a pointer to an int as parameter
 * and updates the value it points to to 98.
 *
 * @n: character to check
@@ -13,5 +13,5 @@
 */
 void reset_to_98(int *n)
 {
-n = 98;
+int *n = 98;
 }
