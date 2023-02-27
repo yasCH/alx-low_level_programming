1e88006d7835180a2a149027ff61612780361da4
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 09:26:57 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
+/**
+* swap_int -Write a function that swaps the values of two integers.
+*
+* @a: character to check
+* @b: character to check
+*
+* Return: (void)
+*/
+void swap_int(int *a, int *b)
+{
+*a = b;
+*b = a;
+}
