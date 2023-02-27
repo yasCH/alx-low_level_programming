7bd70b51d2d8fa2c0dbe9b3323cc6bb0e5c04e34
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 15:22:11 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
+/**
+* rev_string - Write a function that reverses a string.
+*
+* @s: character to check
+*
+* Return: (void)
+*/
+void rev_string(char *s)
+{
+int length = strlen(s);
+int i;
+for (i = length - 1 ; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+}
