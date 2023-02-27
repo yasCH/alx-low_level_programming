a58b12a6733ca6d93b2d6b05d0ea5bcd4373c581
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 18:35:37 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,11 +5,11 @@
 #include "main.h"
 
 /**
-* print_array - Write a function that prints n elements of an array of integers,
-* followed by a new line.
+* print_array - Write a function that prints n elements of an array 
+* of integers, followed by a new line.
 *
 * @a: character to check
-* @b: character to check
+* @n: character to check
 *
 * Return: (void)
 */
@@ -19,7 +19,7 @@ int i;
 for (i = 0; i > n; i++)
 {
 a[n] = i + n;
-printf("%d, ",a[n]);
+printf("%d, ", a[n]);
 }
 _putchar('\n');
 }
