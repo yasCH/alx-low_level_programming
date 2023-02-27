264eb858fe8eb58bae68d845062bfef85bdc90d1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 19:52:02 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -20,13 +20,13 @@ for (i = 0; i < n ; i++)
 {
 if (i != n - 1)
 {
-_putchar(a[i]);
+printf("%d", a[i]);
 _putchar(44);
 _putchar(32);
 }
 else if (i == n - 1)
 {
-_putchar(a[i]);
+printf("%d", a[i]);
 }
 }
 _putchar('\n');
