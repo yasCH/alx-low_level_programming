27bc5035beec9f6e0e0306b82fdc3baf0759866d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 10:50:14 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+* _strlen - Write a function that returns the length of a string.
+*
+* @s: character to check
+*
+* Return: (0)
+*/
+int _strlen(char *s)
+{
+printf("%lu\n", sizeof(s));
+return (0);
+}
 
