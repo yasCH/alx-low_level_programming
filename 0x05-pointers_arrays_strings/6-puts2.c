8b85158cf55922e5345a3f80742cb0e3f9c44ec9
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 17:02:57 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
-* puts2 - Write a function that prints every other character of a string, 
+* puts2 - Write a function that prints every other character of a string,
 * starting with the first character, followed by a new line.
 *
 * @str: character to check
