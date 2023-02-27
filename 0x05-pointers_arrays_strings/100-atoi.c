6b027a59be1101be1b179cb23060557d9eebb290
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-27 22:46:45 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
+/**
+* _atoi - Write a function that prints a string, in reverse,
+* followed by a new line.
+*
+* @s: character to check
+*
+* Return: (int)
+*/
+int _atoi(char *s)
+{
+atoi(s);
+return (s);
+}
