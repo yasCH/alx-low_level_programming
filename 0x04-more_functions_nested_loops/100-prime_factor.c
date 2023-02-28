3b89597ec92869f9178ee5f1ce0330fd90fa0be0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-28 18:51:54 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <unistd.h>
-#include "main.h"
 
 /**
-**Prototype- Prototype practicing
+**main - Entry point
 *
-* Description:
+* Description: Write a program that finds and prints the largest prime factor
+* of the number 612852475143, followed by a new line.
 *
-* @n
-*
-* Return:
+* Return: Always 0 (success)
 */
-
-function
+int main(void)
 {
-
+int n = 612852475143;
+int i;
+if (n % i != 0)
+{
+for (i = 2 ; i <= n; i++)
+{
+printf("%i/n", i);
+}
+}
 }
