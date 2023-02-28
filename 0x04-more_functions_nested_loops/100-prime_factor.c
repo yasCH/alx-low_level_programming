86b2a8f63f38d7f7e4521c7c0985582b3cb39b34
yasCH <43391964+yasCH@users.noreply.github.com>
2023-02-28 20:54:23 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,11 +15,11 @@
 */
 int main(void)
 {
-int n = 612852475143;
+int n = sqrt(612852475143);
 int i;
-for (i = 3; i <= sqrt(n); i++)
+for (i = 3; i <= n; i++)
 {
-if (n % i == 0)
+if (n**2 % i == 0)
 {
 n = n / i;
 }
