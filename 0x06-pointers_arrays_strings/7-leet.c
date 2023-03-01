7784ea290b1520659a9a8a857d1cb17c982c3d8e
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 15:28:27 +0100

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: Write a function that encodes a string into 1337.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+
+}
