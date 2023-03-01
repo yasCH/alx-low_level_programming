b83bbc0d28bfa801358197dd0960c3e2cde7c3ae
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 13:54:44 +0100

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+
+* _strncpy - Write a function that copies a string.
+*
+*@dest to check
+*@src to check
+*@n to check
+*
+* Returns a pointer to the resulting string dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
