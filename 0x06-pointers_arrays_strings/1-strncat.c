ff86363649b1a07fed3e3b515761f8748da1b21b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 13:52:57 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+
+* _strncat - Write a function that concatenates two strings.
+*
+*@dest to check
+*@src to check
+*@n to check
+*
+* Returns a pointer to the resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
