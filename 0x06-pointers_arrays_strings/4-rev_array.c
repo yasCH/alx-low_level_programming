b36720817420675a673368dead7cc880483647e1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 14:08:05 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+
+* reverse_array - Write a function that reverses the content
+*of an array of integers.
+*
+*@a to check
+*@n to check
+*
+* Returns a pointer to the resulting string dest
+*/
+void reverse_array(int *a, int n)
+{
+
+}
