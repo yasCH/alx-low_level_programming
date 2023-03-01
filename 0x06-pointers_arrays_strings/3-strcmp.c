4a97fdf4884e7ca5dd4acb60ff16c1c0d5a9c1e3
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 14:06:20 +0100

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcmp - Write a function that compares two strings.
+*
+*@s1 to check
+*@s2 to check
+*
+* Return 0
+*/
+int *_strcmp(char *s1, char *s2)
+{
+
+}
