f92d20f71d4c8cf76b0bcf0168c7098fd1eb2e1b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 00:33:16 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,18 +13,18 @@ char *_strcat(char *dest, char *src)
 {
 int i = 0;
 int j = 0;
-while(dest[i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
 j = i;
 i = 0;
-while(src[i] != '\0')
+while (src[i] != '\0')
 {
 dest[j] = src[i];
 i++;
 j++;
 }
 dest[j] = '\0';
-return dest;
+return (dest);
 }
