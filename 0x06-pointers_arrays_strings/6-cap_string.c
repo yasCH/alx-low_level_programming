9ae9b1d3a972c74a01c6c0bc0f6cb1864ba465c3
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-01 15:26:38 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* cap_string - Write a function that capitalizes all words of a string.
+*
+* Returns a pointer to the resulting string dest
+*/
+char *cap_string(char *)
+{
+
+}
