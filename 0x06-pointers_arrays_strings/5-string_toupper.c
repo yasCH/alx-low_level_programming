3e8be5e9b7672fc58ce60e50eec90577c1619b53
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 17:56:36 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,16 +10,18 @@
 * of a string to uppercase.
 *
 * @str: to check
+*
 * Return: str
 */
 char *string_toupper(char *str)
 {
-
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
+{
 str[i] =(int)str[i] - 32;
+}
 }
 return (str);
 }
