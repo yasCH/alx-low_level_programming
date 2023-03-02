1b5f53993a9cd56006369e31a4e77ff931a4d58a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 01:08:57 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,9 +8,9 @@
 /**
 * _strncat - Write a function that concatenates two strings.
 *
-*@dest: to check
-*@src: to check
-*@n: to check
+* @dest: to check
+* @src: to check
+* @n: to check
 *
 * Returns: dest.
 */
