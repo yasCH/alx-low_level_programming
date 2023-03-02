617b3a5c62a50a340cf52bbd364961a137b6f9cc
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 01:49:40 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,11 @@
 /**
 * _strncpy - Write a function that copies a string.
 *
-*@dest to check
-*@src to check
-*@n to check
+*@dest: to check
+*@src: to check
+*@n: to check
 *
-* Returns a pointer to the resulting string dest
+* Return: a pointer to the resulting string dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
