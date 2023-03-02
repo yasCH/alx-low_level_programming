7cfdc5b59a88d455e28b8afa2cabbb9ef2c7dec0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 21:28:48 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,23 +15,17 @@
 char *leet(char *s)
 {
 int i;
+int j;
+a1[]="aAeEoOtTlL";
+a2[]="4433007711";
 for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] == 'e' || s[i] == 'E')
+for (j = 0; j <10; j++)
 {
-s[i] = '3';
+if (s[i] == a1[j])
+{
+s[i] = a2[j];
 }
-else if (s[i] == 'o' || s[i] == 'O')
-{
-s[i] = '0';
-}
-else if (s[i] == 't' || s[i] == 'T')
-{
-s[i] = '7';
-}
-else if (s[i] == 'l' || s[i] == 'L')
-{
-s[i] = '1';
 }
 }
 return (s);
