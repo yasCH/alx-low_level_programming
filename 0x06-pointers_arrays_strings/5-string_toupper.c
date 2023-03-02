3933087ec1f57d2c5637e2c3242331162ee017f9
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 17:54:22 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,17 @@
 * string_toupper - Write a function that changes all lowercase letters
 * of a string to uppercase.
 *
-* Return: 0
+* @str: to check
+* Return: str
 */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-toupper();
+
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] =(int)str[i] - 32;
+}
+return (str);
 }
