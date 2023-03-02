89a19fff8941d0cf57c840cc081092c8e331e4c5
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 17:55:02 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
+char *string_toupper(char *str);
 char *cap_string(char *);
 int _putchar(char c);
