5a1f45de8538ff9cdfc5d1aab8e350938d1dd437
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-02 15:01:51 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,16 @@
 *@s1: to check
 *@s2: to check
 *
-* Return: 0
+* Return: n
 */
 int *_strcmp(char *s1, char *s2)
 {
-strcmp(s1, s2);
-return (0);
+int n = 0;
+while (*s1 != '\0' || *s2 != '\0')
+{
+s1++;
+s2++;
+n = (s1 - s2);
+}
+return (n);
 }
