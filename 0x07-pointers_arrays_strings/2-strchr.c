c0ab07ee08bd5e6bee3969966907c3cbe15c1ca1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-06 16:03:35 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+* _strchr - function that locates a character in a string.
+*
+*@s: to check
+*@c: to check
+*
+* Return: c
+*/
+char *_strchr(char *s, char c)
+{
+strchr(s, c);
+return (c);
+}
