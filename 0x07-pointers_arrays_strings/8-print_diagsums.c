c9b5fd1936203649d49940ca7bf29a271a063f59
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-06 21:41:43 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+* print_chessboard - Write a function that prints the chessboard.
+*
+*@a: to check
+*@size: to check
+*
+* Return: void
+*/
+void print_diagsums(int *a, int size)
+{
+int r = 0;
+int i;
+int j;
+for(i=0; i<size; i++)
+{
+for(j=0; j<size; j++)
+{
+if (i==j)
+{
+int rij=a[i][j];
+r= r + rij;
+printf ("%d ",r);
+}
+}
+}
+}
