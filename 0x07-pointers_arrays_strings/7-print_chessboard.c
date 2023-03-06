71695f1f28eee24f1d728cec6335133369439add
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-06 17:14:40 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+* print_chessboard - Write a function that prints the chessboard.
+*
+*@a: to check
+*
+* Return: 0
+*/
+void print_chessboard(char (*a)[8])
+{
+int i;
+for (int i=0; i<=8; i++)
+{
+printf("%d",a[i];
+}
+return (0);
+}
