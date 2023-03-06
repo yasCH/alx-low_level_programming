145afb73812124f70dbd03a449b94ead21b99b2a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-06 17:01:16 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+* _strstr - function that locates a substring.
+*
+*@haystack: to check
+*@needle: to check
+*
+* Return: p
+*/
+char *_strstr(char *haystack, char *needle);
+{
+char *p;
+p= strstr(haystack, needle);
+if (p != 0)
+{
+return (p);
+}
+else
+{
+return (NULL);
+}
+}
+}
