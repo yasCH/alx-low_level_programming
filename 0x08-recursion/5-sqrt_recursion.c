f0ff46e583dd170a74e8cb045c030269b0d1cb8d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-08 20:16:49 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,19 @@
+#include "main.h"
+#include <math.h>
 
+
+/**
+* _sqrt_recursion - Write a function that returns the natural square
+* root of a number.
+*
+* @n: to check
+*
+* Return: -1 fail
+*
+*/
+int _sqrt_recursion(int n)
+{
+int i;
+i= sqrt(n);
+return (i);
+}
