c0447cf699f6f0d403eca1b1a5d02dc533c3a9bb
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-08 18:48:57 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+* factorial - Write a function that returns the factorial 
+* of a given number.
+*
+* @n: to check
+*
+* Return: v
+*/
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1)
+}
+int v =  n * !(n - 1);
+return (v);
+}
