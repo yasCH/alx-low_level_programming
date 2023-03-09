bd3c66064671de8ef02dd1165d85625cb3e9d1fd
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-09 02:19:05 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,13 +13,16 @@
 */
 int _sqrt_recursion(int n)
 {
-int i;
-for (i = 1; i < n; i++)
+if (n <= 0)
 {
-if (n / i == i)
-{
-return (i);
-}
-}
 return (-1);
+}
+if (n == 1)
+{
+return (1);
+}
+if (n > 1);
+{
+return (sqrt(n));
+}
 }
