abe822662c53f0e165b32cb3dd81b64f2e424f8d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-09 02:50:04 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,29 +11,29 @@
 * Return: -1 fail
 *
 */
-int _sqrt_helper(int n, int i) 
+int _sqrt_helper(int n, int i)
 {
 if (i * i == n)
 {
-return i;
-} 
+return (i);
+}
 else if (i * i > n)
 {
-return -1;
+return (-1);
 }
 else
 {
-return _sqrt_helper(n, i+1);
+return (_sqrt_helper(n, i+1));
 }
 }
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
-return -1;
+return (-1);
 }
 else
 {
-return _sqrt_helper(n, 0);
+return (_sqrt_helper(n, 0));
 }
 }
