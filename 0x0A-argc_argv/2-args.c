48a91ae06ef00a0886908797050bb2d6e4aaeec2
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 23:05:39 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+* main - Write a program that prints all arguments it receives.
+*
+* @argc: to check
+* @argv: to check
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);       
+}
+return (0);
+}
