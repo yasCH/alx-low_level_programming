270d1ce1b41c26b4546a2d7f83ced5a67d3c33a0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-11 00:10:47 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,25 @@
 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Write a program that adds positive numbers.
+*
+* @argc: to check
+* @argv: to check
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+int n = argv[1];
+n = n + argv[i];
+printf("%i\n",n );
+}
+return (0);
+}
