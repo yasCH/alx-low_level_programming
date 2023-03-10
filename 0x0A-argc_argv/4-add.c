0c0fcd0c3b78ade9feadd7b439fc994280da0008
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-11 00:11:58 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 int i;
 for (i = 0; i < argc; i++)
 {
-int n = argv[1];
-n = n + argv[i];
+int n = *argv[1];
+n = n + *argv[i];
 printf("%i\n",n );
 }
 return (0);
