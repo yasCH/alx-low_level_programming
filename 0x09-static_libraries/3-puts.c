9479153382b1f9eff03ab73b0e765df4c7472333
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 11:03:42 +0100

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _puts - Write a function that prints a string, followed by a new line,
+* to stdout.
+*
+* @str: character to check
+*
+* Return: (void)
+*/
+void _puts(char *str)
+{
+puts(str);
+}
