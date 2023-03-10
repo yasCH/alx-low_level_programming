bdae64cf3a2dd2ae37a7f672fc24f612cb2f8de9
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 11:19:18 +0100

    Create 100-atoi.c

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _atoi - Write a function that convert a string to an integer.
+*
+* @s: character to check
+*
+* Return: (0)
+*/
+int _atoi(char *s)
+{
+atoi(s);
+return (0);
+}
