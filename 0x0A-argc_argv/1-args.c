617ed2214aae19566ce4a8f2c732b81d851486a7
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 22:10:30 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+* main - Write a program that prints its name, followed by a new line.
+*
+* @argc: to check
+* @argv: to check
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argc);
+return (0);
+}
