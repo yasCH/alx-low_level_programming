ed356326765ca410a0784ccf77c44516ce11f6ec
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 23:14:35 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+* main - Write a program that prints its name, followed by a new line.
+*
+* @argc: to check
+* @argv: to check
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 2)
+{
+printf("%i\n", (argv[1] * argv[2]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
