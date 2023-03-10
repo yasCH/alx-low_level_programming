7772dfbfb32542f97d17769385229a16b3a821f8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-10 11:02:19 +0100

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - Write a function that returns the length of a string.
+*
+* @s: character to check
+*
+* Return: (0)
+*/
+int _strlen(char *s)
+{
+int l;
+l = strlen(s);
+return (l);
+}
