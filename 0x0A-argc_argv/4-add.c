8369f6c625fb2c2c546852ed1b91716975577358
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-11 15:50:26 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,6 +15,7 @@
 int main(int argc, char *argv[])
 {
 int m = 0;
+int a = atoi(argv[i]);
 if (argc < 2)
 {
 printf("0\n");
@@ -24,14 +25,14 @@ else
 int i;
 for (i = 1; i < argc; i++)
 {
-if (argv[i] >= 48 && argv[i] <= 57)
+if (a <= 0)
 {
 printf("Error\n");
 return (1);
 }
 else
 {
-m = m + atoi(argv[i]);
+m = m + a;
 }
 }
 printf("%i\n", m);
