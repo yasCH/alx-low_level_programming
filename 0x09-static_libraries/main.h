cd57dedd897fb966b24c73284c73a09469bc0652
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-11 16:08:05 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
-#IFNDEF
+#ifndef myFunction
+#define myFunction
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,4 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#ENDIF
+#endif
