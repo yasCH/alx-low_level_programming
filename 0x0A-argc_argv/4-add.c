e58d32befcf4e104681c48b707fa0f279a473476
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-11 17:45:16 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,8 +26,8 @@ for (i = 1; i < argc; i++)
 {
 int a = atoi(argv[i]);
 int l = strlen(argv[i]);
-int p = _pow_recursion(10, l-1);
-if (a <= 0 && a < p)
+int p = _pow_recursion(10, l - 1);
+if (a <= 0 || a < p)
 {
 printf("Error\n");
 return (1);
