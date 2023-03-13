693cecb97225b51b5a5e1ed333644ac942381c5e
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-13 20:51:59 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+* create_array - Write a function that creates an array of chars,
+* and initializes it with a specific char.
+*
+* @zize: to check
+* @c: to check
+*
+* Return: a pointer to the array, or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+if( size == 0)
+{
+retun (NULL);
+}
+return (size[c]);
+}
