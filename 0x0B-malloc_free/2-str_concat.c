e10f2e596b0244b5178ad32b78008be6cc77aef4
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-14 17:54:35 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+* create_array - Write a function that creates an array of chars,
+* and initializes it with a specific char.
+*
+* @s1: to check
+* @s2: to check
+*
+* Return: a pointer to the array, or NULL if it fails
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+concat = malloc(sizeof(s1) + sizeof(s2));
+if (concat == NULL)
+{
+return (NULL);
+} 
+else
+{
+return (concat);
+}
+}
