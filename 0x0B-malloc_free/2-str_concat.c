b6b8091a468255c965886f5a3c0143bb5d1c967a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-14 19:29:00 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,6 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
-char *free ="";
 concat = malloc(sizeof(*s1) + sizeof(*s2) + 1);
 if (concat == NULL)
 {
@@ -25,7 +24,7 @@ else
 {
 if (s1 == NULL && s2 == NULL)
 {
-return (free);
+return (0);
 }
 else if (s1 == NULL)
 {
