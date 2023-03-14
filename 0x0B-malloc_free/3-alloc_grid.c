9399e8f646c46a3453af1bfed0efe4c2620439e9
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-14 20:18:23 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 int i;
 int j;
 int **creat;
-creat = malloc(sizeof(width) + sizeof(height));
+creat = malloc(sizeof(width) * sizeof(height));
 if (creat == NULL || width <= 0 || height <= 0)
 {
 return (NULL);
