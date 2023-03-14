ccb0be6e8a7266b32bf30ad3a2d892d3b52a58c6
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-14 16:43:18 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,7 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *creat;
+creat = malloc(sizeof(char) * size);
 if (size == 0)
 {
 return (NULL);
@@ -25,7 +26,6 @@ for (i = 0; i < size ; i++)
 {
 creat[i] = c;
 }
-creat = malloc(sizeof(char) * size);
 return (creat);
 }
 }
