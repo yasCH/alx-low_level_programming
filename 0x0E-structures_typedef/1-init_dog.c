6ba70ad6b5bccbe128f019a36e156e9f5375552b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-20 21:42:01 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,6 +15,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog;
+d->name = malloc(strlen(name) + 1);
+d->owner = malloc(strlen(owner) + 1);
+strcpy(d->name, name);
+d->age = age;
+strcpy(d->owner, owner);
 return (0);
 }
