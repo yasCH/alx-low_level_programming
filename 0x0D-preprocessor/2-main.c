384879359d0efdc2bc1a106914c173f2ae5b70c1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-20 17:46:07 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+fprintf(__FILE__)
+return (0);
+}
