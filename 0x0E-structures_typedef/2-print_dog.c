5e5e0d9b9c7f442566e95b5c6ab07f42eba9b015
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 00:33:51 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - Write a function that prints a struct dog
+*
+* @d: pointer to my dog
+*
+* Return: void
+*/
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+d.name = name;
+d.age;
+d.owner;
+}
+}
 
