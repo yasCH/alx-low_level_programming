480cc2f4b17e006228d36c72c8118d18130ded5c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-20 19:23:47 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+/**
+ * struct dog - dog informations
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ *
+ * Description:dog informations 
+ */
 struct dog
 {
 char *name;
