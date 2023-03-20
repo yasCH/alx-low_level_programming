da30c60a5004e3a02f8a69e9b2b815ba68f82d94
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 00:04:42 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+if (d != NULL && name != NULL && age != NULL && owner != NULL)
 {
 d->name = name;
 d->age = age;
