8593f778ebb597805ffcf9022159cd36dbf4407a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 00:37:23 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,8 @@
 */
 void print_dog(struct dog *d)
 {
-if (d != NULL)
-{
-d.name = name;
-d.age;
-d.owner;
-}
+printf("Name: "%s\n", d->name);
+printf("Age: "%i\n", d->age);
+printf("Owner: "%s\n", d->owner);
 }
 
