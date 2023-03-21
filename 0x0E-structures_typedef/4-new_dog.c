e8c95dafb1af8055c059eca39dfc06caffb7eb5b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-22 00:44:51 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,8 +25,8 @@ strcpy(t->name, name);
 }
 else
 {
+free(t->name);
 free(t);
-return (NULL);
 }
 t->owner = malloc(strlen(owner) + 1);
 if (owner != NULL)
@@ -35,9 +35,8 @@ strcpy(t->owner, owner);
 }
 else
 {
-free(t->name);
+free(t->owner);
 free(t);
-return (NULL);
 }
 t->age = age;
 return (t);
