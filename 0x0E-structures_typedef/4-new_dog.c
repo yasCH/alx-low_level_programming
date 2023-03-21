9a7e725d5d8396542ae2320d68d112f3c3627481
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 03:43:47 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,13 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *t;
-t= malloc(sizeof(*name) + sizeof(age) + sizeof(*owner));
+t = malloc(sizeof(*name) + sizeof(age) + sizeof(*owner));
 if (t == NULL)
 {
-return(NULL);
+return (NULL);
 }
 else
 {
-return(t);
+return (t);
 }
 }
