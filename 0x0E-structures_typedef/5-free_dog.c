87cd5b997840b9909f45bad2e1103c1814b62a64
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 08:55:57 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
 
+/**
+* free_dog - Write a function that frees dogs.
+*
+* @d: pointer to my dog
+*
+* Return: void
+*/
+void free_dog(dog_t *d)
+{
+free(d);
+}
