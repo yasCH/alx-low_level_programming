9feb9c017644d0359f3114305e5c137aa2a85895
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-21 01:38:45 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
 
+/**
+* new_dog - Write a function that creates a new dog.
+*
+* @name: of the dog
+* @age: of the dog
+* @owner: of the dog
+*
+* Return: pointer to dig_t or NULL if it fails
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *t;
+t= malloc(sizeof(char *name) + sizeof(float age) + sizeof(char *owner));
+if (t == NULL)
+{
+return(NULL);
+}
+else
+{
+return(t);
+}
+}
