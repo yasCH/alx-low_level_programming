5f2951261f5cfd6fd887d7e2abaad54da9e3f339
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-22 19:52:34 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if((array == NULL) || (cmp == NULL) || (size <= 0)
+if((array == NULL) || (cmp == NULL) || (size <= 0))
 {
 return (-1);
 }
