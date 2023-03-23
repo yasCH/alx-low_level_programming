e50372187ebda9f7e096670d9dbfbc7beff08c6d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-23 15:32:41 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,14 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+int sum;
 if(n != 0)
 {
 va_list ptr;
 va_start(ptr, n);
-va_arg(ptr, int);
+sum + 1 = va_arg(ptr, int);
 va_end(ptr);
+return(sum);
 }
 return (0);
 }
