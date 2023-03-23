d8bb84329f5fba1d5f1599c383ae236dfd6752a8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-23 16:15:40 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
+/**
+* print_numbers - Function that  prints numbers, followed by a new line.
+*
+* @n: to check
+*
+* Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+
+}
