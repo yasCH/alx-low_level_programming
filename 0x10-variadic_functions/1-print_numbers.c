b8c8407ba460be2c4cd6f44ede19651910abdf3b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-23 18:30:28 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,8 +20,7 @@ va_start(ptr, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ptr, unsigned int));
-printf("%s", separator);
-if (i < n - 1 && separator != NULL) 
+if (i < n - 1) 
 {
 printf("%s", separator);
 }
