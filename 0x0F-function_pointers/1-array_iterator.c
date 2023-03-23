f7078e40231e104898b706f9018e24ee35acbae1
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-23 13:34:41 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-long unsigned int i;
+unsigned long int i;
 if ((array != NULL) && (action != NULL) && (size > 0))
 {
 for (i = 0; i < size; i++)
