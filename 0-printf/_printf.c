a8061062fb4dd2bbc2e11eceed2df8e3d38a1f08
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-24 18:45:21 +0100

    Update _printf.c

--- 0-printf/_printf.c
+++ 0-printf/_printf.c
@@ -1,54 +1,69 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - Write a function that produces output according to a format.
+ *
+ * @format: is a character string.
+ *
+ * Return: number of characters printed excluding "\0"for strings.
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+va_list args;
+int printed_chars = 0;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            ++format;
-            switch (*format) {
-                case 'c':
-                    {
-                        const char c = (char)va_arg(args, int);
-                        putchar(c);
-                        ++printed_chars;
-                    }
-                    break;
+while (*format != '\0') {
+if (*format == '%') {
+++format;
+switch (*format) {
 
-                case 's':
-                    {
-                        const char* s = va_arg(args, char*);
-                        while (*s != '\0') {
-                            putchar(*s++);
-                            ++printed_chars;
-                        }
-                    }
-                    break;
+case 'c':
+{
+const char c = (char)va_arg(args, int);
+_putchar(c);
+++printed_chars;
+}
+break;
 
-                case '%':
-                    {
-                        putchar('%');
-                        ++printed_chars;
-                    }
-                    break;
+case 's':
+{
+const char* s = va_arg(args, char*);
+while (*s != '\0') {
+_putchar(*s++);
+++printed_chars;
+}
+}
+break;
 
-                default:
-                    /* Unsupported conversion specifier. */
-                    return -1;
-            }
-        }
-        else {
-            putchar(*format);
-            ++printed_chars;
-        }
-        ++format;
-    }
+case '%':
+{
+_putchar('%');
+++printed_chars;
+}
+break;
 
-    va_end(args);
-    return printed_chars;
+case 'i':
+{
+const int i = va_arg(args, int);
+_putchar(i);
+++printed_chars;
+}
+break;
+
+default:
+return (0);
+}
+}
+else {
+_putchar(*format);
+++printed_chars;
+}
+++format;
+}
+
+va_end(args);
+return printed_chars;
 }
