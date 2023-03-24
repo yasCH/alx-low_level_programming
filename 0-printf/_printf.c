3befb8f76025fa82e395d38ba61cb17953663f3e
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-24 18:21:19 +0100

    Update _printf.c

--- 0-printf/_printf.c
+++ 0-printf/_printf.c
@@ -3,52 +3,52 @@
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars = 0;
+    va_list args;
+    int printed_chars = 0;
 
-va_start(args, format);
+    va_start(args, format);
 
-while (*format != '\0') {
-if (*format == '%') {
-++format;
-switch (*format) {
-case 'c':
-{
-const char c = (char)va_arg(args, int);
-putchar(c);
-++printed_chars;
-}
-break;
+    while (*format != '\0') {
+        if (*format == '%') {
+            ++format;
+            switch (*format) {
+                case 'c':
+                    {
+                        const char c = (char)va_arg(args, int);
+                        putchar(c);
+                        ++printed_chars;
+                    }
+                    break;
 
-case 's':
-{
-const char* s = va_arg(args, char*);
-while (*s != '\0') {
-putchar(*s++);
-++printed_chars;
-}
-}
-break;
+                case 's':
+                    {
+                        const char* s = va_arg(args, char*);
+                        while (*s != '\0') {
+                            putchar(*s++);
+                            ++printed_chars;
+                        }
+                    }
+                    break;
 
-case '%':
-{
-putchar('%');
-++printed_chars;
-}
-break;
+                case '%':
+                    {
+                        putchar('%');
+                        ++printed_chars;
+                    }
+                    break;
 
-default:
-/* Unsupported conversion specifier. */
-return -1;
-}
-}
-else {
-putchar(*format);
-++printed_chars;
-}
-++format;
-}
+                default:
+                    /* Unsupported conversion specifier. */
+                    return -1;
+            }
+        }
+        else {
+            putchar(*format);
+            ++printed_chars;
+        }
+        ++format;
+    }
 
-va_end(args);
-return printed_chars;
+    va_end(args);
+    return printed_chars;
 }
