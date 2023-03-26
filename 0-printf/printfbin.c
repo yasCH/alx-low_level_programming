7bb1ee1d3c4d6d7eab33e74da742ab2564375e71
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-26 23:36:33 +0200

    Create printfbin.c

---
+++ 0-printf/printfbin.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * printfbin - convert int to binary.
+ * @b: number to convert.
+ * Return: number of printed caracters.
+ */
+
+int printfbin(unsigned int b)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, n;
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & b);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			n = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
