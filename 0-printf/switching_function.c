9ce6741b123302204058f2fb37073083ae94fb5c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-27 02:03:42 +0200

    Create switching_function.c

---
+++ 0-printf/switching_function.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* switching_function - external swith fucnction
+* @format: an array
+* @printed_chars: int
+* @arg: va_list
+* Return: printed_chars
+*/
+int switching_function(const char *format, int printed_chars, va_list arg)
+{
+char c;
+char *s;
+int d;
+switch (*format)
+{
+case 'c':
+c = va_arg(arg, int);
+putchar(c);
+printed_chars++;
+break;
+case 's':
+s = va_arg(arg, char*);
+if (s == NULL)
+{
+return (printed_chars);
+}
+printed_chars += strlen(s);
+fputs(s, stdout);
+break;
+case '%':
+putchar('%');
+printed_chars++;
+break;
+case 'd':
+case 'i':
+d = va_arg(arg, int);
+printed_chars += convert_number(d, 0);
+break;
+default:
+putchar('0');
+}
+return (printed_chars);
+}
