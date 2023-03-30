8c80381a6297b97e95366ce535024e9e09a1f17d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 19:54:14 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,11 +13,12 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+list_t *new_node;
 if (str == NULL)
 {
 return (NULL);
 }
-list_t *new_node = malloc(sizeof(struct(list_t));
+new_node = malloc(sizeof(struct(list_t));
 if (new_node == NULL)
 {
 return (NULL);
