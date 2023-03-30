45d01544065a59b50f2a14ecbdb176f4986833af
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 01:34:32 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,8 +14,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
-char *new_str;
-if (str == NULL)
+if (str == NULL || head == NULL)
 {
 return (NULL);
 }
@@ -26,13 +25,12 @@ if (new_node == NULL)
 {
 return (NULL);
 }
-new_str = strdup(str);
+new_node->str = strdup(str);
 if (new_str == NULL)
 {
 free(new_node);
 return (NULL);
 }
-new_node->str = new_str;
 new_node->next = *head;
 *head = new_node;
 return (new_node);
