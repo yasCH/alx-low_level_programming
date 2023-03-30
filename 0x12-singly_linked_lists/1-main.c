b1f72b30f3bfbee23487e51dc2e65eb4ab9f7df8
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 17:38:00 +0200

    Update and rename 0-main.c to 1-main.c

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -26,15 +26,9 @@ int main(void)
     new->len = 5;
     new->next = head;
     head = new;
-    n = print_list(head);
+    n = list_len(head);
     printf("-> %lu elements\n", n);
-
-    printf("\n");
     free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
