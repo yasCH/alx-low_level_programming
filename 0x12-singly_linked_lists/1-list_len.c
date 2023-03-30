e2d97ae6f797b616e84c4efc016bb9f3b6768d4d
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 16:00:03 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+ * print_list - Print all the elements of a list_t list.
+ * @list_t: 
+ *
+ * Return: The number of nodes
+ */
+ size_t print_list(const list_t *h)
+{
+if (str == NULL)
+{
+printf("[0] (nil)\n");
+}
+}
