50e1d3170645c2b82d91ca9fc6d952ba1626abcd
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 15:43:09 +0200

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,4 +8,19 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+   struct list_s *next;
+} list_t;
+
 #endif
