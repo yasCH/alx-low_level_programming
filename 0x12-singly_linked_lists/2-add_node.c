a48fa05a59e63716784be407a86b4aa6e95cdc95
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 01:20:54 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stddef.h>
 #include "lists.h"
 
 /**
-*add_node - add a new node at the beginning of a list_t list.
-*@head: adress.
-*@str: stirng.
-*Return: the address of the new element, or NULL if it failed.
+* add_node - add a new node at the beginning of a list_t list.
 *
+* @head: adress.
+* @str: stirng.
+*
+* Return: the address of the new element, or NULL if it failed.
 */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -23,18 +23,18 @@ return (NULL);
 }
 else
 {
-if (new_node != NULL)
+if (new_node == NULL)
 {
-new_node->str = new_str;
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
 return (NULL);
+}
 if (new_str == NULL)
 {
 free(new_node);
 return (NULL);
 }
+new_node->str = new_str;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
 }
