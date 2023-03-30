f00d977f681282b4961198b02185d4ad72dfe04e
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 17:09:58 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,20 @@
  */
  size_t print_list(const list_t *h)
 {
+size_t number;
+number = 0;
+while (h != NULL)
+{
 if (h -> str != NULL)
 {
 printf("[%lu] %s\n", h-> len, h-> str);
 }
+else
+{
 printf("[0] (nil)\n");
-return (0);
+}
+number++;
+h = h-> next;
+}
+return (number);
 }
