8844e471bceab96784991ec62de27318ae8f1cd6
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-30 15:37:07 +0200

    Create lists.h

---
+++ lists.h
@@ -0,0 +1,11 @@
+#ifndef _LISTS_H
+#define_LISTS_H
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
