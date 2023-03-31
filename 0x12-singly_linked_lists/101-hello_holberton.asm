4422464842d8c0928c119ba2bfa46a8ccdcbfa9b
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 04:15:59 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void __attribute__((constructor)) before_main()
+void __attribute__((constructor)) calledFirst();
+void calledFirst()
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
