31fa5bfc0b2ca8ddf4e6fd44675b8609e0c4aac0
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 04:13:48 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
-* __attribute__ 
-*
-* Return: void
-*/
 void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\n");
