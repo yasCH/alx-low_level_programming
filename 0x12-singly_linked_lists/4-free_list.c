8f22228f6203516006b287ea9b876b49f04e51f6
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 02:59:00 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+* free_list - Write a function that frees a list_t list.
+*
+* @head: adress.
+*
+* Return: void
+*/
+void free_list(list_t *head)
+{
+free(head);
+}
