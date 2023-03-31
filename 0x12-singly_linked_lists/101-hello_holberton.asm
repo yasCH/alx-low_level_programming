e3f10dced708642d6b9cfd908f103e94a9d5c996
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 04:09:13 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
 * __attribute__ 
@@ -10,5 +8,6 @@
 */
 void __attribute__((constructor)) before_main()
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n);
+printf("I bore my house upon my back!\n");
 }
