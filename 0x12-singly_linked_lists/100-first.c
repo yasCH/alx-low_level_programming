d6507d989450ea4244022947d2370cfaee8c8118
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 04:33:24 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,13 +7,6 @@
 * Return: void
 **/
 void __attribute__((constructor)) calledFirst();
-
-/**
-* calledFirst - function 
-*
-* Return: void
-**/
-void calledFirst()
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
