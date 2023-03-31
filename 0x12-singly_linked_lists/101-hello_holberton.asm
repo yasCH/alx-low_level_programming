0e4b6c746c14516fe62ce9ed3c9e2b803330db8a
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-31 04:17:50 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,6 +4,5 @@
 void __attribute__((constructor)) calledFirst();
 void calledFirst()
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
