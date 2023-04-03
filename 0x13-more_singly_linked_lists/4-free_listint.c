b37802df7522a408adaac4b6608dc2225f354748
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-03 20:00:41 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+* free_listint - Write a function that frees listint_t.
+*
+* @head: adress.
+*
+* Return: void
+*/
+ void free_listint(listint_t *head)
+ {
+ while (head != NULL)
+ {
+ free(head);
+ head = head->next;
+ }
+ }
