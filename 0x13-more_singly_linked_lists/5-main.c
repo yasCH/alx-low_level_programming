7cf83148f74642430e6b51df0bbb558e320696a2
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-04 17:59:39 +0200

    Update and rename 3-main.c to 5-main.c

--- 0x13-more_singly_linked_lists/5-main.c
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -22,5 +22,7 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
     return (0);
 }
