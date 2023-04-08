bad569e813c9f46f85ff33647ead348e81bb6123
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-08 16:24:33 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,5 +8,18 @@
  */
 void print_binary(unsigned long int n)
 {
-
+int i;
+unsigned int binary;
+unsigned int bit;
+for (i = 0; i < 32; i++)
+{
+binary = 0;
+bit = 1;
+if (n != 0)
+{
+binary |= (1 <<=1);
+}
+bit <<= 1;
+}
+printf("%u\n", binary);
 }
