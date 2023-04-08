4ef00f61df4a0ef8bc7f2a46b7a0ef5a74ae0ae4
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-08 02:01:57 +0200

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 int _putchar(char c);
+int _pow(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
