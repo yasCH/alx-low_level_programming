f1aa08c41b8c4763c49ebc24a8ef7c5522a63761
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-08 02:31:30 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * print_binary - print the binary representation of a number.
+ * @n: int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+
+}
