c827dc38cdb4aac8ecaf48b09b8878eaf2e4da05
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-08 02:14:42 +0200

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,43 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-#include <string.h>
-
 int _putchar(char c);
-int _pow(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
-/**
-* _pow - Write a function that returns the value of x
-* raised to the power of y.
-*
-*@x: to check
-*@y: to check
-*
-*Return: n
-*
-*/
-int _pow(int x, int y)
-{
-if (y < 0)
-{
-return (-1);
-}
-if (y == 0)
-{
-return (1);
-}
-else
-{
-int pow;
-pow = x * _pow(x, y - 1);
-return (pow);
-}
-}
 
 #endif
