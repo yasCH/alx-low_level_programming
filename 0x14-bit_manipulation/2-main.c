c31bd1bdf4415602c088a03f1b596ab374a31f48
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-09 16:40:56 +0200

    Update 2-main.c

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -12,6 +12,10 @@ int main(void)
 
     n = get_bit(1024, 10);
     printf("%d\n", n);
+    n = get_bit(7, 12);
+    printf("%d\n", n);
+    n = get_bit(0, 0);
+    printf("%d\n", n);
     n = get_bit(98, 1);
     printf("%d\n", n);
     n = get_bit(1024, 0);
