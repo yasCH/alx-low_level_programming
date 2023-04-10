7504d5d117026bfe9a90dfdfa1ba9456bcc97a28
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-10 23:48:19 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Create a function that creates a file.
+ * @filename: the file name
+ * @text_content: null terminated string
+ * Return: 1 on success -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+if (filename == NULL && text_content == NULL)
+{
+return (-1);
+}
+creat(filename,S_IRWXG);
+return (1);
+}
