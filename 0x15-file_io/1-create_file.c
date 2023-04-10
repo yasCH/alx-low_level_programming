f970cac70ff5b7d1be9eaa8a76d20f0c88615a44
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-11 01:56:22 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+int fd;
 size_t writer;
 int creator;
 if (filename == NULL || text_content == NULL)
 {
 return (-1);
 }
+fd = open(filename, O_RDWR);
+if (fd < 0)
+{
+return (-1);
 creator = creat(filename, S_IRWXG);
 if (creator < 0)
 {
@@ -30,7 +35,9 @@ return (-1);
 writer = write(STDOUT_FILENO, text_content, strlen(text_content));
 if (writer < 0 || writer != sizeof(text_content))
 {
+close(fd);
 return (-1);
 }
+close(fd);
 return (1);
 }
