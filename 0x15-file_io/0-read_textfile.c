1115604ae2d9404425511707c09c74695d92a888
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-10 17:04:53 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
 
+/**
+ * read_textfile - check the code
+ * @filename: the file name
+ * @letters: the number of letters to read & print.
+ * Return: n of letters or 0 if filename is NULL, write fails, file cant o&r.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+if (filename == NULL)
+{
+return (0);
+}
+fd = open(filename; O_RDONLY);
+}
