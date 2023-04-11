3b34fc15fd4033c9ad45a4e048e1444db3a4f5fb
yasCH <43391964+yasCH@users.noreply.github.com>
2023-04-11 02:51:42 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ if (text_content != NULL)
 {
 writer = write(fd, text_content, strlen(text_content));
 }
-if (writer < 0 || writer < sizeof(text_content))
+if (writer < 0 || writer != sizeof(text_content))
 {
 close(fd);
 return (-1);
